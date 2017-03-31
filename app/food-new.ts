0b2b1a8469a2f684f568856216140682f2ecb516
Minh Phuong <mphuong@kent.edu>
2017-03-31 10:19:56 -0700

    add form to input new food and ts file

---
+++ app/food-new.ts
@@ -0,0 +1,16 @@
+import { Component, Output, EventEmitter } from "@angular/core";
+import { Food } from './model';
+
+@Component({
+  selector: 'food-new',
+  templateUrl: './app/food-new.html'
+})
+
+export class FoodNewComponent {
+  @Output() submitSend = new EventEmitter();
+
+  submit(name:string, details: string, calories: number){
+    var newFood = new Food(name, details, calories);
+    this.submitSend.emit(newFood);
+  }
+}
